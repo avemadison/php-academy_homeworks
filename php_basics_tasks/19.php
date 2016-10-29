ecb6b23f0fc4e7ff6e9145de3721fc4c15c2b24c
Sofi <avemadison@rambler.ru>
2016-10-29 18:24:11 +0300

    reworked

--- php_basics_tasks/19.php
+++ php_basics_tasks/19.php
@@ -1,13 +1,4 @@
 <?php
-$a = "78";
-$b = 78;
-if($a != $b) {
-    echo "Не равны";
-}
-else {
-    echo "Равны";
-};
-echo '<br>';
-var_dump($a);
-var_dump($b);
-?>
+$a='78';
+$b=78;
+echo var_dump($a===$b);
