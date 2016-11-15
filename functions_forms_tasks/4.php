797aa2651596c9605009132a192fb922ce355a81
Sofi <avemadison@rambler.ru>
2016-11-15 10:59:45 +0200

    add v2

--- functions_forms_tasks/4.php
+++ functions_forms_tasks/4.php
@@ -20,3 +20,28 @@ function fileDir($dir){
 if($_POST){
     dd(fileDir($dir));
 }
+
+//Variant 2
+
+function show_dir($dir_name)
+{
+    $d = opendir($dir_name);       // . - текущая директория   .. - родительская
+    while ($name = readdir($d)) {  //чтоб просмотреть все используем цикл. (readdir по одному элементу выводит)
+        if ($name == "." or $name == "..")  //чтоб не выводить [.] и [..]
+            continue;
+        if (is_dir($name))         //Определяет, является ли имя файла директорией
+            echo "<b>[$name]</b> - папка <br>";
+        else
+            echo "$name<br>";
+    }
+    closedir($d); // сразу пишем закрытие после открытия. чтоб не забыть
+}
+$dir_name = '.';
+echo <<<  "HTML"
+<pre>
+ * 4. Написать функцию, которая выводит список файлов в заданной директории.
+ * Директория задается как параметр функции.
+<pre>
+Выведем список в директории '{$dir_name}'
+HTML;
+show_dir($dir_name);
